9a3bef42c81309c7312ca18a0e38f4b6a5e0a49f
s26234-pj <s26234@pjwstk.edu.pl>
2024-01-27 17:22:54 +0100

    init

---
+++ src/main/java/plp/pjatk/mj/s26234bank/S26234BankApplication.java
@@ -0,0 +1,13 @@
+package plp.pjatk.mj.s26234bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S26234BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S26234BankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/plp/pjatk/mj/s26234bank/S26234BankApplicationTests.java
@@ -0,0 +1,13 @@
+package plp.pjatk.mj.s26234bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S26234BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
