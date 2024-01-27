a21f394cac3ebf6e7aba5a4a641bc83a1083e2d7
s26234-pj <s26234@pjwstk.edu.pl>
2024-01-27 17:24:41 +0100

    git test

--- src/main/java/plp/pjatk/mj/s26234bank/S26234BankApplication.java
+++ src/main/java/plp/pjatk/mj/s26234bank/S26234BankApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class S26234BankApplication {
 
+    public S26234BankApplication(){
+        System.out.println("git test");
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(S26234BankApplication.class, args);
     }
